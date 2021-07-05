11ced94281b925a7bfef462ee666668fe35363bb
OleksandrRudkin <85992438+OleksandrRudkin@users.noreply.github.com>
2021-07-05 17:08:49 +0300

    Add files via upload

---
+++ LessOrMore/src/main/java/ua/rudkin/lessormore/Controller.java
@@ -0,0 +1,56 @@
+package main.java.ua.rudkin.lessormore;
+
+import java.util.Scanner;
+
+public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void processUser() {
+        Scanner sc = new Scanner(System.in);
+
+        model.setSecretValue();
+
+        view.printMessage(View.GREETINGS);
+
+        while (model.checkValue(inputIntValueWithScanner(sc)));
+
+        view.printMessage(View.CONGRATULATION + model.getSecretValue());
+        view.printMessage(View.TRY_LIST + String.valueOf(model.getYourWay()));
+    }
+
+    private int inputIntValueWithScanner(Scanner sc) {
+        int res = 0;
+        view.printMessage(getInputIntMessage());
+        while (true) {
+            // check int-value
+            while (!sc.hasNextInt()) {
+                view.printMessage(View.WRONG_INPUT_INT_DATA + getInputIntMessage());
+                sc.next();
+            }
+            // check value into diapason
+            if ((res = sc.nextInt()) <= model.getMinBarrier() ||
+                    res >= model.getMaxBarrier()) {
+                view.printMessage(View.WRONG_INPUT_INT_DATA + getInputIntMessage());
+                continue;
+            }
+            break;
+        }
+        return res;
+    }
+
+    private String getInputIntMessage() {
+        return view.concatenationString(
+                View.INPUT_INT_DATA, View.OPENS_SQUARE_BRACKET,
+                String.valueOf(model.getMinBarrier()), View.SPACE_SING,
+                String.valueOf(model.getMaxBarrier()),
+                View.CLOSING_SQUARE_BRACKET, View.SPACE_SING,
+                View.EQUAL_SING, View.SPACE_SING );
+    }
+
+}

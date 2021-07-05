0ab022a73c0c90261c49ec94be1ea6b6098e713b
OleksandrRudkin <85992438+OleksandrRudkin@users.noreply.github.com>
2021-07-05 17:07:44 +0300

    Add files via upload

---
+++ HelloWorldMVC/src/main/java/ua/rudkin/helloworldmvc/Main.java
@@ -0,0 +1,12 @@
+package main.java.ua.rudkin.helloworldmvc;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+
+        controller.processUser();
+    }
+}

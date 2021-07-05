2992265084cbef731988848eeb9778c9d6a5b475
OleksandrRudkin <85992438+OleksandrRudkin@users.noreply.github.com>
2021-07-05 17:12:54 +0300

    Add files via upload

---
+++ RegistrationForm/src/main/java/ua/rudkin/registrationform/Controller.java
@@ -0,0 +1,13 @@
+package main.java.ua.rudkin.registrationform;
+
+public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+
+}

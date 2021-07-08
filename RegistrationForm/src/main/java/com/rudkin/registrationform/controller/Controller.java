258df9decd544fe63ef22ece54f2ecf6d5a5d383
Oleksandr Rudkin <oleksandr.rudkin@gmail.com>
2021-07-08 17:15:12 +0300

    change View, Controller, Main

--- RegistrationForm/src/main/java/com/rudkin/registrationform/controller/Controller.java
+++ RegistrationForm/src/main/java/com/rudkin/registrationform/controller/Controller.java
@@ -1,5 +1,25 @@
 package main.java.com.rudkin.registrationform.controller;
 
+import main.java.com.rudkin.registrationform.model.Model;
+import main.java.com.rudkin.registrationform.view.View;
+
+import java.util.Scanner;
+
 public class Controller {
 
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void processUser() {
+        Scanner sc = new Scanner(System.in);
+        InputNoteNoteBook inputNoteNoteBook =
+                new InputNoteNoteBook(view, sc);
+        inputNoteNoteBook.inputNote();
+    }
 }
+
---
+++ RegistrationForm/src/main/java/com/rudkin/registrationform/view/View.java
@@ -0,0 +1,41 @@
+package main.java.com.rudkin.registrationform.view;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static main.java.com.rudkin.registrationform.view.TextConstant.INPUT_STRING_DATA;
+import static main.java.com.rudkin.registrationform.view.TextConstant.WRONG_INPUT_DATA;
+
+public class View {
+
+    static String MESSAGES_BUNDLE_NAME = "messages";
+    public static final ResourceBundle bundle =
+            ResourceBundle.getBundle(MESSAGES_BUNDLE_NAME,
+                //   new Locale("ua", "UA"));
+                   new Locale("en"));
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public String concatenationString(String... message) {
+              StringBuilder concatString = new StringBuilder();
+              for(String v : message) {
+                  concatString = concatString.append(v);
+              }
+              return new String(concatString);
+    }
+
+    public void printStringInput(String message) {
+        printMessage(concatenationString(
+                bundle.getString(INPUT_STRING_DATA),
+                bundle.getString(message)));
+    }
+
+    public void printWrongStringInput(String message) {
+        printMessage(concatenationString(
+                bundle.getString(WRONG_INPUT_DATA),
+                bundle.getString(INPUT_STRING_DATA),
+                bundle.getString(message)));
+    }
+}

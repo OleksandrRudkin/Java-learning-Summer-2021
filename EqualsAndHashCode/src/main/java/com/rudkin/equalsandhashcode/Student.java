d6180e1491cc7de110ce0b3e20a6c076230e9dae
Oleksandr Rudkin <oleksandr.rudkin@gmail.com>
2021-07-11 14:13:29 +0300

    add Student

---
+++ EqualsAndHashCode/src/main/java/com/rudkin/equalsandhashcode/Student.java
@@ -0,0 +1,68 @@
+package main.java.com.rudkin.equalsandhashcode;
+
+
+public class Student {
+    private String name;
+    private int age;
+    private String group;
+
+    public Student(String name, int age, String group) {
+        this.name = name;
+        this.age = age;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (age != student.age) return false;
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        return group != null ? group.equals(student.group) : student.group == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        result = 31 * result + (group != null ? group.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", group='" + group + '\'' +
+                '}';
+    }
+}
